ee636fb1b499785c91ea81a5598f9da1f9b062b4
shangweihung <alannn1230@gmail.com>
2020-03-26 17:41:58 -0700

    Updates

---
+++ precache-manifest.aab110677e04cb84bfe854d30fbb0380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97ccea22b8a98703432b1c079bfb74f",
+    "url": "/money-in-pocket/index.html"
+  },
+  {
+    "revision": "4d79608e81a945d0576c",
+    "url": "/money-in-pocket/static/css/main.0e5bb76b.chunk.css"
+  },
+  {
+    "revision": "25942417c6f93ad93aca",
+    "url": "/money-in-pocket/static/js/2.02e37928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/money-in-pocket/static/js/2.02e37928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d79608e81a945d0576c",
+    "url": "/money-in-pocket/static/js/main.ec5c1b4d.chunk.js"
+  },
+  {
+    "revision": "6e51d1144673e123ead8",
+    "url": "/money-in-pocket/static/js/runtime-main.1fa55366.js"
+  }
+]);
